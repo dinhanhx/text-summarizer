d8c1661453d725eeaa8816c4c7900a9f7cab308a
dinhanhx <dinhanhx@gmail.com>
2020-03-13 21:51:52 +0700

    A new wikipedia test code, test case and implemented shiity algorithms for processing wikipedia

--- app/TestCode.py
+++ app/TestCode.py
@@ -1,9 +1,24 @@
 import scriptum_abberivator as sa
+import textus_abberivator as ta
 
-text_file = open('A_long_text.txt', 'r')
-text = text_file.read()
-text_file.close()
+## Test raw file text
+# text_file = open('A_long_text.txt', 'r')
+# text = text_file.read()
+# text_file.close()
 
-text_sum = sa.summary(text)
+# out_text_file = open('A_long_text_sum.txt', 'r')
+# out_text_file.write(sa.summary(text))
+# out_text_file.close()
 
-print(text_sum)
+
+## Test a wikipedia url
+# url = 'https://en.wikipedia.org/wiki/Science_of_Logic'
+# out_text_file = open('A_wiki_sum.txt', 'w')
+# out_text_file.write(ta.summary(url))
+# out_text_file.close()
+
+## Note
+# import nltk
+# nltk.download('stopwords')
+# nltk.download('punkt')
+
