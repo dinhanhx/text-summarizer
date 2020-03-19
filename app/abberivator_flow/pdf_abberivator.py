3447d48e92b940340ca8d6f9d219e63df6aec3cc
dinhanhx <dinhanhx@gmail.com>
2020-03-19 21:25:10 +0700

    Delete corrupted .pdf file, create new sample pdf file, implement pdf_abberivator.py

---
+++ app/abberivator_flow/pdf_abberivator.py
@@ -0,0 +1,15 @@
+from abberivator_flow import scriptum_abberivator as sa
+import PyPDF2
+
+def summarize(pdf_file):
+	pdfFileObj = open(pdf_file, 'rb')
+	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+	num = pdfReader.numPages
+	text = []
+	for i in range(num):
+		pageObj = pdfReader.getPage(i)
+		text.append(pageObj.extractText())
+
+	print(text)
+	seperator = ', '
+	return sa.summarize(seperator.join(text))
--- app/test_code_and_case/TestCode.py
+++ app/test_code_and_case/TestCode.py
@@ -1,12 +1,13 @@
 from abberivator_flow import scriptum_abberivator as sa
 from abberivator_flow import wikipedia_abberivator as wa
+from abberivator_flow import pdf_abberivator as pa
 
 ## Test raw file text
 # text_file = open('A_long_text.txt', 'r')
 # text = text_file.read()
 # text_file.close()
 
-# out_text_file = open('A_long_text_sum.txt', 'r')
+# out_text_file = open('A_long_text_sum.txt', 'w')
 # out_text_file.write(sa.summary(text))
 # out_text_file.close()
 
@@ -15,6 +16,12 @@ from abberivator_flow import wikipedia_abberivator as wa
 # url = 'https://en.wikipedia.org/wiki/Science_of_Logic'
 # out_text_file = open('A_wiki_sum.txt', 'w')
 # out_text_file.write(ta.summary(url))
+# out_text_file.close()
+
+## Test a pdf file
+# pdf_file = 'sample.pdf'
+# out_text_file = open('A_pdf_sum.txt', 'w')
+# out_text_file.write(pa.summarize(pdf_file))
 # out_text_file.close()
 
 ## Note
