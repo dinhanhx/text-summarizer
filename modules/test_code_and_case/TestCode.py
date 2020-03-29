10d8f81237c1c3a69175e50ae94a2fae438b6101
dinhanhx <dinhanhx@gmail.com>
2020-03-29 10:37:28 +0700

    Not Important

--- modules/test_code_and_case/TestCode.py
+++ modules/test_code_and_case/TestCode.py
@@ -11,14 +11,14 @@ from abberivator_flow import pdf_abberivator as pa
 # text_file.close()
 
 # out_text_file = open('A_long_text_sum.txt', 'w')
-# out_text_file.write(sa.summary(text))
+# out_text_file.write(sa.summarize(text))
 # out_text_file.close()
 
 
 ## Test a wikipedia url
 # url = 'https://en.wikipedia.org/wiki/Science_of_Logic'
 # out_text_file = open('A_wiki_sum.txt', 'w')
-# out_text_file.write(ta.summary(url))
+# out_text_file.write(wa.summarize(url))
 # out_text_file.close()
 
 ## Test a pdf file
