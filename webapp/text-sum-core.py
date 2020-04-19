0074d8c37df22365cfa9b54c980951d895f0074b
DDHLong <48487369+DDHLong@users.noreply.github.com>
2020-04-19 21:09:17 +0700

    Update text-sum-core.py
    ReLOAD

--- webapp/text-sum-core.py
+++ webapp/text-sum-core.py
@@ -43,4 +43,5 @@ def result_pdff():
 		# return pa.summarize(f.filename)
 
 if __name__ == '__main__':
-   app.run(debug = False, host= '0.0.0.0')
+	app.config["TEMPLATES_AUTO_RELOAD"] = True
+	app.run(debug = False, host= '0.0.0.0')
