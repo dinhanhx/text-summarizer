f7d6bebc2d8405a5e11e0f37d8ca5f9cde74d299
dinhanhx <dinhanhx@gmail.com>
2020-05-01 09:23:57 +0700

    Rename check_extension() to get_extension(). #35

--- modules/abberivator_flow/document_abberivator.py
+++ modules/abberivator_flow/document_abberivator.py
@@ -4,12 +4,12 @@ from abberivator_flow import odt_abberivator as odt_a
 
 import os
 
-def check_extension(file_path):
+def get_extension(file_path):
     extension = os.path.splitext(file_path)[-1]
     return extension.replace(".", "")
 
 def summarize(file_path):
-    extension = check_extension(file_path)
+    extension = get_extension(file_path)
     extension_list = ['odt', 'docx', 'pdf']
     if extension not in extension_list:
     	return "File not supported"
